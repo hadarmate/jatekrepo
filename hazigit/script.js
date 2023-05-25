3128d5e8e4a8e573fa7f0b9cc1db0402d6086dbe
hadarmate <114560486+hadarmate@users.noreply.github.com>
2023-05-25 09:38:58 +0200

    Teljes weboldal

---
+++ hazigit/script.js
@@ -0,0 +1,7 @@
+function showContent(id) {
+    var contentDivs = document.querySelectorAll('.content div');
+    for (var i = 0; i < contentDivs.length; i++) {
+      contentDivs[i].style.display = 'none';
+    }
+    document.getElementById(id).style.display = 'block';
+  }
