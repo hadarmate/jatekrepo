aa16b7dc293ed442d45c1040f4363af0f4c1fa66
hadarmate <114560486+hadarmate@users.noreply.github.com>
2023-05-25 09:39:21 +0200

    Delete hazi directory

--- hazi/script.js
+++
@@ -1,7 +0,0 @@
-function showContent(id) {
-  var contentDivs = document.querySelectorAll('.content div');
-  for (var i = 0; i < contentDivs.length; i++) {
-    contentDivs[i].style.display = 'none';
-  }
-  document.getElementById(id).style.display = 'block';
-}
